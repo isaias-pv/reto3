6a1074c3acc3169cce163e84f4b59d0f810bdf50
IsaiasPv <alemais2003312@gmail.com>
2021-11-11 18:43:29 -0500

    Correción

---
+++ Front-end/js/api.js
@@ -0,0 +1,6 @@
+export const BASE_URL           = "http://129.159.207.189:80/" //<- Mi ip : Mi puerto
+export const URL_CATEGORY       = BASE_URL + "api/Category/"
+export const URL_CLOUD          = BASE_URL + "api/Cloud/"
+export const URL_CLIENT         = BASE_URL + "api/Client/"
+export const URL_MESSAGE        = BASE_URL + "api/Message/"
+export const URL_RESERVATION    = BASE_URL + "api/Reservation/"
---
+++ Front-end/js/reservation.js
@@ -0,0 +1,90 @@
+import { URL_RESERVATION, URL_CLOUD, URL_CLIENT } from "./api.js";
+
+function postReservation(){
+    $.ajax({
+        url :  URL_RESERVATION + "save",
+        type:   "POST",
+        data:   JSON.stringify({
+            startDate: document.getElementById("startDate").value,
+            devolutionDate: document.getElementById("startDate").value,
+            cloud: {id: $("#idCloud").val()},
+            client: {idClient: $("#idClient").val()}
+        }),
+        contentType:"application/JSON",
+        datatype: "JSON",
+        success:() => {
+            alert("Reserva guardada")
+            getReservations()
+        }
+    });
+}
+
+function loadOptions(){
+    $.ajax({
+        url :   URL_CLOUD + "all",
+        type:   "GET",
+        datatype:   "JSON",
+        success:(response) => {
+            response.forEach(element => {
+                let option = document.createElement("option")
+                option.innerHTML = element.name
+                option.value = element.id
+                $("#idCloud").append(option)
+            });
+        }
+    });
+
+    $.ajax({
+        url :   URL_CLIENT + "all",
+        type:   "GET",
+        datatype:   "JSON",
+        success:(response) => {
+            response.forEach(element => {
+                let option = document.createElement("option")
+                option.innerHTML = element.name
+                option.value = element.idClient
+                $("#idClient").append(option)
+            });
+        }
+    });
+}
+
+function getReservations(){
+    $.ajax({
+        url :   URL_RESERVATION + "all",
+        type:   "GET",
+        datatype:   "JSON",
+        success:(response) => {
+            loadReservation(response)
+        }
+    });
+}
+
+function loadReservation(items){
+    let myTable = document.getElementsByTagName("loadReservations")
+
+    for(let i = 0; i < items.length; i++){
+
+        myTable+="<tr>";
+        myTable+="<td>"+items[i].idReservation+"</td>";
+        myTable+="<td>"+items[i].cloud.name+"</td>";
+        myTable+="<td>"+items[i].client.idClient+"</td>";
+        myTable+="<td>"+items[i].client.name+"</td>";
+        myTable+="<td>"+items[i].client.email+"</td>";
+        myTable+= items[i].client.score == null ? "<td>No tiene calificación</td>" : "<td>"+items[i].client.score+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</tbody>";
+    $("#loadReservations").empty()
+    $("#loadReservations").append(myTable);
+}
+
+$('#postReservation').click(function(){
+    postReservation()
+});
+
+$('#getReservations').click(function(){
+    getReservations()
+});
+
+document.addEventListener("DOMContentLoaded", loadOptions())
